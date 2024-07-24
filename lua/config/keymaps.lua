ac4d64363c5ab0f0b1d6155a0126914ebcfadf36
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2024-07-24 15:24:06 +0200

    feat: restore bufferline navigation keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.del("n", "<S-h>")
-vim.keymap.del("n", "<S-l>")
+-- vim.keymap.del("n", "<S-h>")
+-- vim.keymap.del("n", "<S-l>")
