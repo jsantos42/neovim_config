10f23fe490451bbbc697e0288a1bfa42188b3b96
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2024-10-08 15:09:39 +0200

    feat: add Neotest

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.test.core" },
     { import = "plugins" },
   },
   defaults = {
