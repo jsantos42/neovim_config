e8bdc5ab0ca1f8d988bc0af16c5ff3b4a6a05af7
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2025-07-28 11:01:28 +0100

    fix: move the theme toggling to the appropriate place

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,20 @@
 -- Autocmds are automatically loaded on the VeryLazy event
+
+local function set_colorscheme()
+  if vim.o.background == "light" then
+    vim.cmd.colorscheme "vscode"
+  else
+    vim.cmd.colorscheme "vague"
+  end
+end
+
+-- Set the colorscheme on startup
+set_colorscheme()
+
+-- And update it whenever the 'background' option is changed
+vim.api.nvim_create_autocmd("OptionSet", {
+  pattern = "background",
+  callback = set_colorscheme,
+})
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,23 +1,4 @@
-local function set_colorscheme()
-  if vim.o.background == "light" then
-    vim.cmd.colorscheme "vscode"
-  else
-    vim.cmd.colorscheme "vague"
-  end
-end
-
 return {
   { "vague2k/vague.nvim", priority = 1000 },
   { "Mofiqul/vscode.nvim", priority = 1000 },
-  {
-    "LazyVim/LazyVim",
-    config = function()
-      set_colorscheme()
-
-      vim.api.nvim_create_autocmd("OptionSet", {
-        pattern = "background",
-        callback = set_colorscheme,
-      })
-    end,
-  },
 }
