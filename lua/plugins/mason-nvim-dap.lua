1dea94bb81a172bf4ddf0c3645d9561137b71376
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2025-10-17 14:15:35 +0200

    feat: stop at breakpoints in blade files

--- lua/plugins/mason-nvim-dap.lua
+++ lua/plugins/mason-nvim-dap.lua
@@ -38,5 +38,39 @@ return {
         },
       },
     }
+    dap.configurations.blade = {
+      {
+        name = "Debug ObservadorWordPress",
+        type = "php",
+        request = "launch",
+        port = 9003,
+        -- this is needed for a remote app running in a container
+        pathMappings = {
+          ["/usr/src/wordpress/wp-content"] = "${workspaceFolder}",
+          ["/usr/src/wordpress"] = "${workspaceFolder}/.wordpress-src",
+          ["/tmp/bladecache"] = "${workspaceFolder}/bladecache",
+          ["/usr/src/wordpress/wp-config.php"] = "${workspaceFolder}/.docker/config/wp-config.php",
+        },
+        skipFiles = {
+          "**/wp-includes/**",
+          "**/vendor/**",
+        },
+      },
+    }
   end,
 }
+
+-- XDEBUG CONFIG:
+-- zend_extension = xdebug
+-- xdebug.client_port = 9003
+-- xdebug.client_host = host.docker.internal
+-- xdebug.log = /tmp/xdebug.log
+-- xdebug.cli_color = 1
+-- xdebug.mode = develop,debug
+-- xdebug.start_upon_error = yes
+--
+--
+-- TO STOP IN A BLADE FILE:
+-- @php
+--     xdebug_break();
+-- @endphp
