e93faa06f3cc5bc4a85eca9a2e4ba21a508c1a98
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2025-10-22 15:49:42 +0200

    feat: add completion with Tab

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,19 @@
+-- https://cmp.saghen.dev/installation.html
+return {
+  "saghen/blink.cmp",
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    keymap = {
+      preset = "super-tab",
+    },
+  },
+}
