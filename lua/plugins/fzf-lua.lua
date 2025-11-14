e65fbf3fb7bcf3bc5ca43ad369298c4b1e12cd72
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2025-11-14 13:12:41 +0100

    feat: add extension filter to file search

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -20,14 +20,7 @@ return {
         .. [[-g "!gulp.log" ]]
         .. [[-g "!**/init.sql" ]]
 
-      require("fzf-lua").setup({
-        winopts = {
-          height = 1,
-          width = 1,
-        },
-        files = {
-          formatter = "path.filename_first",
-          fd_opts = [[--color=never --hidden --type f --type l ]]
+      local fd_base_opts = [[--color=never --hidden --type f --type l ]]
             .. [[--exclude "**/.git" ]]
             .. [[--exclude "**/.idea" ]]
             .. [[--exclude "**/.vscode" ]]
@@ -39,9 +32,44 @@ return {
             .. [[--exclude "*min.js*" ]]
             .. [[--exclude "*gulp.log" ]]
             .. [[--exclude "**/node_modules/*" ]],
+
+      require("fzf-lua").setup({
+        winopts = {
+          height = 1,
+          width = 1,
+        },
+        files = {
+          formatter = "path.filename_first",
+          fd_opts = fd_base_opts,
+          actions = {
+            ["alt-e"] = function(_, opts)
+              local ext = vim.fn.input("Filter by extension: ")
+              if ext and ext ~= "" then
+                ext = ext:gsub("^%.", "")
+                local new_opts = vim.tbl_deep_extend("keep", { resume = true }, opts.__call_opts or {})
+                new_opts.fd_opts = fd_base_opts .. " -e " .. ext
+                if opts.__call_fn then
+                  opts.__call_fn(new_opts)
+                end
+              end
+            end,
+          },
         },
         grep = {
           rg_opts = base_rg_opts,
+          actions = {
+            ["alt-e"] = function(_, opts)
+              local ext = vim.fn.input("Filter by extension: ")
+              if ext and ext ~= "" then
+                ext = ext:gsub("^%.", "")
+                local new_opts = vim.tbl_deep_extend("keep", { resume = true }, opts.__call_opts or {})
+                new_opts.rg_opts = base_rg_opts .. " -g '*." .. ext .. "'"
+                if opts.__call_fn then
+                  opts.__call_fn(new_opts)
+                end
+              end
+            end,
+          },
         },
         oldfiles = {
           include_current_session = true,
