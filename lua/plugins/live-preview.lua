3bfb4d9852d5a74c5dfce8c4d20043d533601f5a
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2026-08-31 20:24:14 +0200

    fix: turn HTML LivePreview into a single command operation

--- lua/plugins/live-preview.lua
+++ lua/plugins/live-preview.lua
@@ -8,6 +8,8 @@ return {
 				picker = "fzf-lua",
 			}
 
+			local start_preview
+
 			if vim.env.NVIM_CONTAINERIZED == "1" then
 				cfg.address = "0.0.0.0"
 				cfg.browser = "echo"
@@ -29,21 +31,48 @@ return {
 					return 6674
 				end
 
-				vim.api.nvim_create_user_command("LivePreviewStart", function()
+				start_preview = function(filepath)
 					local port = pick_free_port()
 					cfg.port = port
 					require("livepreview.config").set(cfg)
-					vim.cmd("LivePreview start")
-					vim.schedule(function()
-						local relpath = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":.")
-						local url = ("http://127.0.0.1:%d/%s"):format(port, relpath)
-						vim.fn.setreg("+", url)
-						vim.notify("Live preview URL copied!\nPaste into the browser.", vim.log.levels.INFO)
-					end)
-				end, {})
+
+					local lp = require("livepreview")
+					local utils = require("livepreview.utils")
+					local Config = require("livepreview.config").config
+					local fs = vim.fs
+
+					filepath = fs.normalize(filepath or vim.api.nvim_buf_get_name(0))
+					if not lp.start(filepath, port) then
+						return
+					end
+
+					local urlpath = Config.dynamic_root and fs.basename(filepath)
+						or utils.get_relative_path(filepath, fs.normalize(vim.uv.cwd() or ""))
+					local url = ("http://127.0.0.1:%d/%s"):format(port, vim.uri_encode(urlpath))
+					vim.fn.setreg("+", url)
+					vim.notify("Live preview URL copied!\nPaste into the browser.", vim.log.levels.INFO)
+				end
+			else
+				start_preview = function(filepath)
+					filepath = vim.fs.normalize(filepath or vim.api.nvim_buf_get_name(0))
+					require("livepreview").start(filepath)
+				end
 			end
 
 			require("livepreview.config").set(cfg)
+
+			vim.api.nvim_create_user_command("LivePreview", function(opts)
+				local sub = opts.fargs[1]
+				if not sub or sub == "start" then
+					start_preview(opts.fargs[2])
+				elseif sub == "close" then
+					require("livepreview").close()
+				elseif sub == "pick" then
+					require("livepreview").pick()
+				else
+					require("livepreview").help()
+				end
+			end, { force = true, nargs = "?" })
 		end,
 	},
 }
